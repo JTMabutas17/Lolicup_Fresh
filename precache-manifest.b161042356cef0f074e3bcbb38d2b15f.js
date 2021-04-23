6537f01a553a19192480ccca49e83c9fdaa47d18
JTMabutas17 <thisjuicetin@gmail.com>
2021-04-23 15:01:58 -0700

    Updates

---
+++ precache-manifest.b161042356cef0f074e3bcbb38d2b15f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7026512877a5b5b5ba0b352c8857e512",
+    "url": "/Lolicup_Fresh/index.html"
+  },
+  {
+    "revision": "849e404627bb34d88cc2",
+    "url": "/Lolicup_Fresh/static/css/2.3b8df3f8.chunk.css"
+  },
+  {
+    "revision": "8b8f9e33ea475acbef3e",
+    "url": "/Lolicup_Fresh/static/css/main.e8986148.chunk.css"
+  },
+  {
+    "revision": "849e404627bb34d88cc2",
+    "url": "/Lolicup_Fresh/static/js/2.f8c7059b.chunk.js"
+  },
+  {
+    "revision": "dbe5ca7a82c728776bb536c3d2d64135",
+    "url": "/Lolicup_Fresh/static/js/2.f8c7059b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b8f9e33ea475acbef3e",
+    "url": "/Lolicup_Fresh/static/js/main.93089708.chunk.js"
+  },
+  {
+    "revision": "ec0e97015531dca42fef",
+    "url": "/Lolicup_Fresh/static/js/runtime-main.70978c61.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.95fc5f01d79413091ff897eae8512ed5.js"
+  "/Lolicup_Fresh/precache-manifest.b161042356cef0f074e3bcbb38d2b15f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Lolicup_Fresh/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
