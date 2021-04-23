066a0388dd280c6526a86ffe5c02c7be24984a37
JTMabutas17 <thisjuicetin@gmail.com>
2021-04-23 05:34:48 -0700

    Updates

---
+++ precache-manifest.d389c51a8ad0da6415617ee91c7e99a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69a4f19cd125a25b60257182a5eefb77",
+    "url": "/Lolicup_Fresh/index.html"
+  },
+  {
+    "revision": "849e404627bb34d88cc2",
+    "url": "/Lolicup_Fresh/static/css/2.3b8df3f8.chunk.css"
+  },
+  {
+    "revision": "dc543eee149333bbbf71",
+    "url": "/Lolicup_Fresh/static/css/main.e8986148.chunk.css"
+  },
+  {
+    "revision": "849e404627bb34d88cc2",
+    "url": "/Lolicup_Fresh/static/js/2.f8c7059b.chunk.js"
+  },
+  {
+    "revision": "dbe5ca7a82c728776bb536c3d2d64135",
+    "url": "/Lolicup_Fresh/static/js/2.f8c7059b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc543eee149333bbbf71",
+    "url": "/Lolicup_Fresh/static/js/main.de7354a8.chunk.js"
+  },
+  {
+    "revision": "ec0e97015531dca42fef",
+    "url": "/Lolicup_Fresh/static/js/runtime-main.70978c61.js"
+  }
+]);
