b64e38e6ad0323dccebaa12c2736cb84137e5e30
JTMabutas17 <thisjuicetin@gmail.com>
2021-04-23 14:49:47 -0700

    Updates

---
+++ precache-manifest.95fc5f01d79413091ff897eae8512ed5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc7e65b5f8ab35b5dcd5a66d514390f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f308253aba06bf8af61a",
+    "url": "./static/css/2.3b8df3f8.chunk.css"
+  },
+  {
+    "revision": "8618c2764f8b1618e616",
+    "url": "./static/css/main.e8986148.chunk.css"
+  },
+  {
+    "revision": "f308253aba06bf8af61a",
+    "url": "./static/js/2.62590f80.chunk.js"
+  },
+  {
+    "revision": "dbe5ca7a82c728776bb536c3d2d64135",
+    "url": "./static/js/2.62590f80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8618c2764f8b1618e616",
+    "url": "./static/js/main.189aa3b5.chunk.js"
+  },
+  {
+    "revision": "9c428731bbfdd79aab0e",
+    "url": "./static/js/runtime-main.6329dfa0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Lolicup_Fresh/precache-manifest.d389c51a8ad0da6415617ee91c7e99a4.js"
+  "./precache-manifest.95fc5f01d79413091ff897eae8512ed5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Lolicup_Fresh/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
